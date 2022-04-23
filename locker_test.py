add08fc7e473d04da6acc88b5eb8aee6cf1ec72f
waynemorphic <wayneklyde@gmail.com>
2022-04-23 16:08:09 +0300

    added test methods for the credentials class

--- locker_test.py
+++ locker_test.py
@@ -1,6 +1,8 @@
 from locker import User
 import unittest
+from locker import Credentials
 
+# Testig the cases for the User class
 class Testuser (unittest.TestCase):
     '''
     class defining cases/methods to test the User class for its behaviours
@@ -46,6 +48,46 @@ class Testuser (unittest.TestCase):
 
         self.new_user.save_user_details()
         self.assertEqual(len(User.user_list),1)
+
+class TestCredentials(unittest.TestCase):
+    '''
+    test cases and methods to test the credentials class
+    '''
+
+    def setUp(self):
+        '''
+        setUp case to run before each case or method
+        '''
+
+        self.new_credentials = Credentials("Twitter", "jdoe", "twitter123")
+        '''
+        constructor to create new instances of the credentials class
+        '''
+
+    def tearDown(self):
+        '''
+        cleans up after every case/method runs
+        '''
+
+        Credentials.credentials_list = []
+    
+    def test_init(self):
+        '''
+        checking for proper credentials class initialization
+        '''
+
+        self.assertEqual(self.new_credentials.platform,"Twitter")
+        self.assertEqual(self.new_credentials.username, "jdoe")
+        self.assertEqual(self.new_credentials.password, "twitter123")
+
+    def test_save_credentials(self):
+        '''
+        method to check if platform credentials are saved
+        '''
+
+        self.new_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 1)
+
 
 print("hello")
 if __name__ ==  '__main__':
