1bcf5a3221b441c96f60558eb33674e13e6f2831
waynemorphic <wayneklyde@gmail.com>
2022-04-23 18:25:50 +0300

    added test cases for delete, search and view credentials for credentials class

--- locker_test.py
+++ locker_test.py
@@ -49,6 +49,7 @@ class Testuser (unittest.TestCase):
         self.new_user.save_user_details()
         self.assertEqual(len(User.user_list),1)
 
+## Credentials class
 class TestCredentials(unittest.TestCase):
     '''
     test cases and methods to test the credentials class
@@ -80,6 +81,7 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.username, "jdoe")
         self.assertEqual(self.new_credentials.password, "twitter123")
 
+## First Credentials test for saving credentials entries
     def test_save_credentials(self):
         '''
         method to check if platform credentials are saved
@@ -87,7 +89,8 @@ class TestCredentials(unittest.TestCase):
 
         self.new_credentials.save_credentials()
         self.assertEqual(len(Credentials.credentials_list), 1)
-    
+
+## Second Credentials test for checking ability to save multiple credentials
     def test_save_multiple_credentials(self):
         '''
         method to test if multiple user credentials can be saved
@@ -98,12 +101,38 @@ class TestCredentials(unittest.TestCase):
         test_credentials.save_credentials()
         self.assertEqual(len(Credentials.credentials_list),2)
 
+## Third Credentials test to test if existing credentials can be deleted from the application
+    def test_delete_credentials(self):
+        '''
+        method to test if credentials are removable
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials ("Twitter", "jdoe", "twitter123")
+        test_credentials.save_credentials
+
+        self.new_credentials.delete_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 0) # Added 0 at the end since Assertion error ocurred saying 0 != 1. Resolved that assertion error
+
+## Fourth Credentials test for returning credentials in the application
     def test_view_credentials(self):
         '''
         method that returns all saved credentials
         '''
 
         self.assertEqual(Credentials.view_credentials(), Credentials.credentials_list)
+
+## Fifth test for searching existing platforms in the application by name
+    def test_search_platform(self):
+        '''test to find a platform by the name given by the user
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("Twitter", "jdoe", "twitter123")
+        test_credentials.save_credentials()
+
+        found_platform = Credentials.search_platform("Twitter")
+        self.assertEqual(found_platform.platform, test_credentials.platform)
 
 print("hello")
 if __name__ ==  '__main__':
