c048e8e9ae7d8f0d7d72d10b706fea3583ea4851
waynemorphic <wayneklyde@gmail.com>
2022-04-24 14:49:17 +0300

    logic for registering new user or accepting to login an existing user

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#! usr/bin/env python3.8
+
 '''
 #! shebang code that makes the script executable without necessarily typing python3.8
 
@@ -23,12 +23,19 @@ def save_account_details(details):
     '''
     details.save_user_details()
 
+# Authenticate existing user details
+def existing_user_authentication(username, password):
+    '''
+    method to authenticate the details of a registered user
+    '''
+    return User.authenticate_user(username, password)
+
 # Input application name and credentials of the application
-def enter_details(application, usernme, passwrd):
+def enter_details(application, username, password):
     '''
     function to enter application name and its credentials to be stored by the application
     '''
-    new_credentials = Credentials(application, usernme, passwrd)
+    new_credentials = Credentials(application, username, password)
     return new_credentials
 
 # Save application credentials
@@ -67,3 +74,64 @@ def random_password(password):
     function to generate a random password from PasswordGenerator module
     '''
     return Credentials.generate_password(password)
+
+
+# Main function for running the password locker application
+def main():
+    print("Hello, welcome to your password locker. What is your name?")
+    person_name = input()
+
+    print(f"Hello { person_name }. What would you like to do?")
+    print('\n')
+
+    while True:
+        print("Use these short codes : reg - Register password locker account, lg - Login account if registered")
+        short_code = input().lower()
+
+        if short_code == 'reg':
+            # Create account
+            print("----- Register Account -----")
+            print('='*30)
+
+            print("First Name")
+            f_name = input()
+
+            print("Last Name")
+            l_name = input()
+
+            print("Account Username")
+            a_username = input()
+
+            print("Account Password")
+            a_password = input()
+
+            # Save user account details
+            save_account_details(register_account(f_name, l_name, a_username, a_password))
+            print('\n')
+            print(f"New password locker account username is { a_username }")
+            print("=" * 30)
+            print('\n')
+
+        elif short_code == 'lg':
+            print("----- Login -----")
+            print('='*30)
+
+            print("Account Username")
+            existing_username = input()
+
+            print("Account Password")
+            existing_password = input()
+
+            # Authenticate the user details 
+            if existing_user_authentication(existing_username, existing_password):
+                print("Login Successful")
+                print("=" * 30)
+
+            print('\n')
+            
+            # User is successfully logged in the account
+            print("What woould you like to do?")
+
+if __name__ == '__main__':
+
+    main()
